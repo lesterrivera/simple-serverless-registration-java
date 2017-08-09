46fe2d71723731859b3dcc7181afa942b009abd8
Lester Rivera <lester@identivize.com>
2017-08-08 23:41:31 -0500

    Fixed serverless.yml issues. Disabled s3sync plugin due to error.

--- src/main/java/com/serverless/ConfirmSubscriberHandler.java
+++ src/main/java/com/serverless/ConfirmSubscriberHandler.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.serverless.data.AuthPolicy;
 import com.serverless.data.Subscriber;
 import com.serverless.models.DynamoDBAdapter;
+import com.serverless.utils.Validation;
 import org.apache.log4j.Logger;
 
 import java.util.Collections;
@@ -17,6 +18,12 @@ public class ConfirmSubscriberHandler implements RequestHandler<Map<String, Obje
 
 	private static final Logger LOG = Logger.getLogger(ConfirmSubscriberHandler.class);
 
+	/**
+	 * Handles the Confirm Subscriber event triggered by the GET /register API call.
+	 * @param input
+	 * @param context
+	 * @return
+	 */
 	@Override
 	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 		LOG.info("received: " + input);
@@ -24,16 +31,31 @@ public class ConfirmSubscriberHandler implements RequestHandler<Map<String, Obje
 		try {
 			// Get the email of the subscriber
 			String principalId = input.get("email").toString();
+			// Get the VerifyToken of the subscriber
+			String verifyToken = input.get("verifyToken").toString();
 
 			// Send to DynamoDB
 			Subscriber user = DynamoDBAdapter.getInstance().getSubscriber(principalId);
 
 			// Ensure that the subscriber has NOT already been verified
 			if (!user.getIsVerified()){
-				// Verify the VerifyHash
-
-				// Update the subscriber to verify
+				// Verify the verifyToken against the user's stored verifyHash
+				if (Validation.validateHash(verifyToken, user.getVerifyHash())) {
+					LOG.info("Subscriber verifyToken is valid");
+					// Update the isVerified value for the user
+					user.setIsVerified(Boolean.TRUE);
+					// Send to DynamoDB
+					DynamoDBAdapter.getInstance().putSubscriber(user);
+					// Add isVerified to response
+					LOG.info("Update subscriber isVerified in database");
+				} else {
+					LOG.info("Subscriber verifyToken is NOT valid");
+				}
+				LOG.info("Subscriber email is already verified");
 			}
+
+			// Add value to the response object
+			input.put("isVerified", user.getIsVerified());
 
 		}catch(Exception e){
 			LOG.error(e,e);
